f36f578dc62c320684d25e9f76985fd75a2856ec
harsh chaturvedi <hchaturvedi@microsoft.com>
2024-12-24 21:22:01 +0530

    Initial Commit

---
+++ DemoAppJivi/DemoAppJiviApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppJiviApp.swift
+//  DemoAppJivi
+//
+//  Created by Harsh Chaturvedi on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppJiviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoAppJiviTests/DemoAppJiviTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppJiviTests.swift
+//  DemoAppJiviTests
+//
+//  Created by Harsh Chaturvedi on 24/12/24.
+//
+
+import Testing
+@testable import DemoAppJivi
+
+struct DemoAppJiviTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
